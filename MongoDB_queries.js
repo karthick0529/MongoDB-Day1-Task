f11e70d781bbae13250cb6fecd36d33359f30ea8
Karthick R <53260064+karthick0529@users.noreply.github.com>
2024-07-09 12:26:24 +0530

    Update MongoDB_queries.js

--- MongoDB_queries.js
+++ MongoDB_queries.js
@@ -179,7 +179,8 @@
         "product_color": "indigo"
     }
 ]
-
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
 // Performing the corresponding MongoDB queries
 
 1. Find all the information about each products
@@ -352,6 +353,8 @@ Guvi> db.product.find({})
 Type "it" for more
 Guvi>
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+
 2. Find the product price which are between 400 to 800
 
 Output:
@@ -401,6 +404,8 @@ Guvi> db.product.find({ product_price: { $gte: 400, $lte: 800 } });
 ]
 Guvi>
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
 3. Find the product price which are not between 400 to 600
 
 Output:
@@ -571,6 +576,8 @@ Guvi> db.product.find({$or: [{product_price:{$lt:400}},{product_price:{$gt:600}}
 Type "it" for more
 Guvi>
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
 4. List the four product which are greater than 500 in price   
 
 Output:
@@ -603,6 +610,8 @@ Guvi> db.product.find({product_price:{$gt:500}}).limit(4)
   }
 ]
 Guvi>
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
 
 5. Find the product name and product material of each products
 
@@ -655,6 +664,8 @@ Guvi> db.product.find({},{ product_name: 1, product_material: 1, _id: 0 }).prett
 Type "it" for more
 Guvi>
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+
 6. Find the product with a row id of 10
 
 Output:
@@ -671,6 +682,8 @@ Guvi> db.product.find({id:'10'})
   }
 ]
 Guvi>
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
 
 7. Find only the product name and product material
 
@@ -723,6 +736,8 @@ Guvi> db.product.find({},{ product_name: 1, product_material: 1, _id: 0 }).prett
 Type "it" for more
 Guvi>
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
+
 8. Find all products which contain the value of soft in product material 
 
 Output:
@@ -763,6 +778,8 @@ Guvi> db.product.find({product_material:'Soft'}).pretty()
   }
 ]
 Guvi>
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
 
 9. Find products which contain product color indigo  and product price 492.00
 
@@ -812,6 +829,8 @@ Guvi> db.product.find({$or:[{product_color: "indigo"}, {product_price: 492}] })
   }
 ]
 Guvi>
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------  
 
 10. Delete the products which product price value are 28
 
